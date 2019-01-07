252bdc1bd5360f3ee8e02020695a71a6200c8845
abdullathanseeh <abdullathanseeh@simelabs.com>
2019-01-07 17:52:42 +0530

    api and crud generic added

---
+++ src/api/crud.js
@@ -0,0 +1,53 @@
+
+
+function getData(data){
+	axios.get(data.url)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+}
+
+function putData(){
+axios.put(data.url, data.params)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function deleteData(){
+axios.delete(data.url, data.params)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function postData(){
+axios.post(data.url, data.params)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+export {
+	getData,
+	putData,
+	postData,
+	deleteData
+}
